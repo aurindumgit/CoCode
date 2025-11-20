b9c482dd61e38d05fa80dfa8b57e235f8e746121
aurindumgit <bose.aurindum2019@gmail.com>
2025-11-20 18:22:14 +0530

    footer change

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -1,9 +1,7 @@
 function Footer() {
     return (
         <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
-            <span>
-                Build with ❤️
-            </span>
+            
         </footer>
     )
 }
