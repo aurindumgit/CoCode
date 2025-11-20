9b59c7e028945772c2b49ae183a2d3b898c72117
aurindumgit <bose.aurindum2019@gmail.com>
2025-11-20 18:17:19 +0530

    removed footer name

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -2,13 +2,7 @@ function Footer() {
     return (
         <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
             <span>
-                Build with ❤️ by{" "}
-                <a
-                    href="https://github.com/aurindumgit"
-                    className="text-blue-500 underline underline-offset-1"
-                >
-                    Auri
-                </a>
+                Build with ❤️
             </span>
         </footer>
     )
